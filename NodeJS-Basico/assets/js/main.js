33be305c8c3c95ef738e6644b119722c48d27185
Danilo de Britto <danilodebritto@Shadow-Hunter.local>
2024-04-22 16:40:26 -0300

    criacao de paginacao da pokedex e estilizacao de cores

--- NodeJS-Basico/assets/js/main.js
+++ NodeJS-Basico/assets/js/main.js
@@ -1,56 +1,67 @@
 
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButto');
+const limit = 5;
+let offset = 0;
 
 
-function convertPokemonToLi(pokemon)
+    //cria os LIs do HTML de cada pokemon
+    //const newList = pokemons.map(convertPokemonToLi(pokemon).join(''))
+
+    /*
+    const newList = pokemons.map((pokemon) => {
+        return convertPokemonToLi(pokemon);
+    }
+    )*/
+
+    //const newHtml = newList.join('')
+
+    //const listItem = [];
+    //debugger;
+    //console.log(pokemonList);
+
+    //cria os LIs do HTML de cada pokemon
+    // for (let i = 0; i< pokemons.length; i++)
+    // {
+    //     const pokemon = pokemons[i];
+    //     //console.log(convertPokemonToLi(pokemon));
+    //     //console.log(pokemon.name);
+                    
+    //     listItem.push(convertPokemonToLi(pokemon));        
+    // }
+
+
+    //console.log(newList);
+
+
+
+function loadPokemonItens(offset, limit)
 {
-    return `
-    <li class="pokemon">
-    <span class="number">#001</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
-        <ol class="types">
-            <li class="type">Grass</li>
-            <li class="type">Poison</li>
-        </ol>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-    </div>
-</li>    
-    `
+        pokeApi.getPokemons(offset, limit).then( (pokemons = []) => 
+        {
+            const newHtml = pokemons.map((pokemon) => 
+                `
+                <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>    
+            `).join('');
+            pokemonList.innerHTML += newHtml
+        }
+    )
+    .catch( (error) => console.error(error));
 }
 
-const pokemonList = document.getElementById('pokemonList');
+loadPokemonItens(offset, limit)
 
-
-
-    pokeApi.getPokemons().then( (pokemons = []) => 
-    {
-        //cria os LIs do HTML de cada pokemon
-        //const newList = pokemons.map(convertPokemonToLi(pokemon).join(''))
-
-        /*
-        const newList = pokemons.map((pokemon) => {
-            return convertPokemonToLi(pokemon);
-        }
-        )*/
-
-        //const newHtml = newList.join('')
-
-        const listItem = [];
-        //debugger;
-        //console.log(pokemonList);
-
-        //cria os LIs do HTML de cada pokemon
-        // for (let i = 0; i< pokemons.length; i++)
-        // {
-        //     const pokemon = pokemons[i];
-        //     //console.log(convertPokemonToLi(pokemon));
-        //     //console.log(pokemon.name);
-                        
-        //     listItem.push(convertPokemonToLi(pokemon));        
-        // }
-
-        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-        //console.log(newList);
-    })
-    .catch( (error) => console.error(error));
-    
+loadMoreButton.addEventListener('click', () => 
+{ 
+    offset += limit
+    loadPokemonItens(offset, limit);
+})
